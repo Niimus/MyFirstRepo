26269522cc6b2962566330062be55181582f0ad7
Martin <poet_martin@yahoo.co.uk>
2014-12-26 09:23:54 +0000

    test

---
+++ ConsoleApplication1/ConsoleApplication1/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Car
+    {
+        public string petName;
+        public int currSpeed;
+
+        public void PrintState()
+        {
+            Console.WriteLine("{0} is going {1} MPH.",petName,currSpeed);
+        }
+
+        public void SpeedUp(int delta)
+        {
+            currSpeed += delta;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Action and Func *****");
+            // Use the Action<> delegate to point to DisplayMessage.
+            Action<string, ConsoleColor, int> actionTarget = new Action<string, ConsoleColor, int>(DisplayMessage);
+            actionTarget("Action Message!", ConsoleColor.Yellow, 5);
+            Console.ReadLine();
+        }
+
+        // This is a target for the Action<> delegate.
+        static void DisplayMessage(string msg, ConsoleColor txtColor, int printCount)
+        {
+            // Set color of console text.
+            ConsoleColor previous = Console.ForegroundColor;
+            Console.ForegroundColor = txtColor;
+            for (int i = 0; i < printCount; i++)
+            {
+                Console.WriteLine(msg);
+            }
+            // Restore color.
+            Console.ForegroundColor = previous;
+            List<int> list = new List<int>{1,2,3,4};
+            List<int> even = list.FindAll(i => (i % 2) == 0);
+        }
+    }
+}
